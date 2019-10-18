2dcca80b6f8d19564224abcc14ed88aea23e367b
James Kirwa <example@email.com>
2019-10-18 22:56:50 +0300

    make changes

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-#naming a url helps us to refer to it from elsewhere in project
-
 urlpatterns =[
     url(r'^$', views.welcome, name = 'welcome'),
     url(r'^today/$', views.awards_of_day, name = 'awardsToday'),
--- news/views.py
+++ news/views.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to yearly Awards')
+    return HttpResponse('Welcome to Yearly Awards')
 
 def awards_of_day(request):
     date = dt.date.today()
@@ -14,7 +14,7 @@ def awards_of_day(request):
     html = f'''
         <html>
             <body>
-                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
+                <h1> Awards for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
