dfdaf3b1ccfa59a409281a723bba9426e68a9cdc
James Kirwa <example@email.com>
2019-10-18 22:10:10 +0300

    create a new view function

--- news/views.py
+++ news/views.py
@@ -5,3 +5,13 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to yearly awards')
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
