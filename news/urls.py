5aa2420b33f6c16934a6c98583d1d47d5bb75b2c
James Kirwa <example@email.com>
2019-10-18 22:03:30 +0300

    add a coment

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+#naming a url helps us to refer to it from elsewhere in project
+
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
 ]
