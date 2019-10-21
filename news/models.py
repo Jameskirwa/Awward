46750999dc7a84ffb7b255f5a4dee8d6f63acb85
James Kirwa <example@email.com>
2019-10-21 14:20:35 +0300

    create class tech

--- news/models.py
+++ news/models.py
@@ -13,4 +13,17 @@ class categories(models.Model):
     @classmethod
     def delete_category(cls,categories):
         cls.objects.filter(categories=categories).delete()
+        
+class technologies(models.Model):
+    technologies = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.technologies
+
+    def save_technology(self):
+        self.save()
+
+    @classmethod
+    def delete_technology(cls,technologies):
+        cls.objects.filter(technologies=technologies).delete()        
 
