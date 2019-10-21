4b9797601bd458cbec87d5b80d10da5f751d8259
James Kirwa <example@email.com>
2019-10-21 14:29:37 +0300

    install pillow

--- news/models.py
+++ news/models.py
@@ -25,5 +25,28 @@ class technologies(models.Model):
 
     @classmethod
     def delete_technology(cls,technologies):
-        cls.objects.filter(technologies=technologies).delete()        
+        cls.objects.filter(technologies=technologies).delete()
+        
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    landing_page = models.ImageField(upload_to='landing/')
+    description = models.CharField(max_length=255)
+    live_link = models.CharField(max_length=255)
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    creativity = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall = models.IntegerField(blank=True,default=0)
+    categories = models.ManyToManyField(categories,blank = True)
+    technologies = models.ManyToManyField(technologies,blank = True)
+    posted  = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_project(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects                
 
